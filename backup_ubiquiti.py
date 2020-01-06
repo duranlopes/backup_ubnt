7eb524f2a6002f92f7ee92759766d133530c5bad
Duran <duran750@hotmail.com>
2020-01-05 22:28:19 -0300

    linux/windows

--- backup_ubiquiti.py
+++ backup_ubiquiti.py
@@ -4,7 +4,16 @@ from scp import SCPClient
 from config import ips, user, password, localfile
 
 path = os.getcwd()
-backup_folder = path +'\\backup'
+
+def bar():
+    if os.name == 'nt':
+        bar = '\\'
+        return bar
+    else:
+        bar = '/'
+        return bar
+
+backup_folder = path + bar() +'backup'
 
 def backup_radio(ip_radio, directory):
     try:
@@ -34,27 +43,14 @@ def rename_file(old_name, new_name):
         os.remove(new_name)
         os.rename(old_name, new_name)
 
-def new_name():
-    if os.name == 'nt':
-        newname = backup_folder +'\\'+ key+'.cfg'
-    else:
-        newname = backup_folder +'/'+ key+'.cfg'
-
-
 def main():
     for key, value in ips.items():
-        if os.name == 'nt':
-            newname = backup_folder +'\\'+ key+'.cfg'
-            backup_file = backup_folder + '\system.cfg'
-        else:
-            newname = backup_folder +'/'+ key+'.cfg'
-            backup_file = backup_folder + '/system.cfg'
+        newname = backup_folder + bar()+ key +'.cfg'
+        backup_file = backup_folder + bar() + 'system.cfg'
         backup_radio(value, backup_folder)
         if os.path.exists(backup_file):
-            #print (newname)
             rename_file(backup_file, newname)
         print('Backup efetuado com sucesso')
-
 
 if __name__ == '__main__':
     main()
